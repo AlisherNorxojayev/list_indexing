d4097633cee72b3537387fc761822507d71336f1
Alisher <norxojayevalisher3@gmail.com>
2022-08-15 12:06:54 +0500

    done

--- bin/list_index10.dart
+++ bin/list_index10.dart
@@ -7,13 +7,16 @@
         list: return answer
 */
 void main() {
-  print(func([1, 2]));
+  List m = [
+    func([1, 2])
+  ];
+  print(m);
 }
 
 int func(List list_num) {
-  int  i = list_num.length - 1;
-  if (list_num[0] > list_num[i]) {
-    return list_num[0];
+  int i = list_num.length - 1;
+  if (list_num.first > list_num.last) {
+    return list_num.last;
   }
   return list_num[list_num.length - 1];
 }
